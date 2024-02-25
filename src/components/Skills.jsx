ea80b67de9ce2472ebb2e6152d7460f2582f92ca
Daniel Hernandez <danielhd94@hotmail.com>
2024-02-24 22:54:35 -0800

    Add stack MERN

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,18 +1,107 @@
 import React from "react";
+import {
+  FaBookmark,
+  FaCode,
+  FaDatabase,
+  FaTerminal,
+  FaGlobe,
+  FaFolder,
+  FaServer,
+} from "react-icons/fa";
+import { useTheme } from "../contexts/ThemeProvider";
+import Skill from "./Skill";
+import { skills } from "../data/skills";
 
 function Skills() {
+  const { isDarkMode } = useTheme();
+
   return (
-    <section id="skills" className="section">
+    <section
+      className={`skills py-5 ${
+        isDarkMode
+          ? "bg-background-dark text-textLight"
+          : "bg-background-light text-textDark"
+      }`}
+      id="skills"
+    >
       <div className="container">
-        <h2>Habilidades</h2>
-        <ul className="skills-list">
-          <li>JavaScript</li>
-          <li>React.js</li>
-          <li>Node.js</li>
-          <li>Express.js</li>
-          <li>MongoDB</li>
-          {/* Agrega más habilidades según sea necesario */}
-        </ul>
+        <Skill skills={skills} />
+        <div className="row">
+          <h2 className="mb-4">Lenguajes de Programación y Frameworks</h2>
+          <div className="col-lg-6 col-12">
+            <div className="skills-list">
+              <h3>Avanzado</h3>
+              <ul className="list-disc pl-4">
+                <li>
+                  <FaCode className="h-5 w-5 inline mr-2" /> Java
+                </li>
+                <li>
+                  <FaServer className="h-5 w-5 inline mr-2" /> C#
+                </li>
+                <li>
+                  <FaCode className="h-5 w-5 inline mr-2" /> JavaScript
+                </li>
+                <li>
+                  <FaFolder className="h-5 w-5 inline mr-2" /> Bootstrap
+                </li>
+                <li>
+                  <FaDatabase className="h-5 w-5 inline mr-2" /> MySQL
+                </li>
+                <li>
+                  <FaDatabase className="h-5 w-5 inline mr-2" /> SQL Server
+                </li>
+              </ul>
+            </div>
+
+            <div className="skills-list">
+              <h3>Intermedio</h3>
+              <ul className="list-disc pl-4">
+                <li>
+                  <FaGlobe className="h-5 w-5 inline mr-2" /> HTML5
+                </li>
+                <li>
+                  <FaGlobe className="h-5 w-5 inline mr-2" /> CSS3
+                </li>
+                <li>
+                  <FaServer className="h-5 w-5 inline mr-2" /> Node.js
+                </li>
+                <li>
+                  <FaBookmark className="h-5 w-5 inline mr-2" /> React.js
+                </li>
+                <li>
+                  <FaGlobe className="h-5 w-5 inline mr-2" /> Git
+                </li>
+                <li>
+                  <FaTerminal className="h-5 w-5 inline mr-2" /> Bash Script
+                  (Unix)
+                </li>
+              </ul>
+            </div>
+
+            <div className="skills-list">
+              <h3>Básico</h3>
+              <ul className="list-disc pl-4">
+                <li>
+                  <FaGlobe className="h-5 w-5 inline mr-2" /> Amazon S3
+                </li>
+                <li>
+                  <FaBookmark className="h-5 w-5 inline mr-2" /> JIRA
+                </li>
+                <li>
+                  <FaFolder className="h-5 w-5 inline mr-2" /> Bitbucket
+                </li>
+                <li>
+                  <FaFolder className="h-5 w-5 inline mr-2" /> GitLab
+                </li>
+                <li>
+                  <FaFolder className="h-5 w-5 inline mr-2" /> Docker
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          {/* Otras secciones */}
+        </div>
       </div>
     </section>
   );
---
+++ src/data/skills.ts
@@ -0,0 +1,51 @@
+interface Skill {
+  name: string;
+  rating: number;
+}
+
+interface SkillsData {
+  advanced: Skill[];
+  intermediate: Skill[];
+  basic: Skill[];
+}
+
+export const skills: SkillsData = {
+  advanced: [
+        { name: "Java", rating: 5
+        },
+        { name: "Spring Boot", rating: 5
+        },
+        { name: ".NET Core", rating: 5
+        },
+        { name: "JavaScript", rating: 5
+        }
+    ],
+  intermediate: [
+        { name: "TypeScript", rating: 3
+        },
+        { name: "NestJS", rating: 3
+        },
+        {
+          name: "MongoDB", rating: 4
+        },
+        {
+          name: "ExpressJS", rating: 3
+        },
+        {
+          name: "ReactJS", rating: 3
+        },
+        {
+          name: "NodeJS", rating: 3
+        },
+    ],
+  basic: [
+        { name: "Apollo GraphQL", rating: 2
+        },
+        { name: "TanStack Query", rating: 2
+        },
+        { name: "Jest", rating: 2
+        },
+        { name: "Amazon S3", rating: 2
+        }
+    ]
+};
